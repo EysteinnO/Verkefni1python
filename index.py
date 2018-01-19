d507bd9f8948da697ac686847eb64e66710c9dab
EysteinnO <eyssi93@gmail.com>
2018-01-19 14:34:25 +0000

    Update index.py

--- index.py
+++ index.py
@@ -6,30 +6,5 @@ def index():
     return "Virkar!"
 
 run(host="0.0.0.0", port=os.environ.get('PORT'))
-#run(host="localhost", port=8080);
-"""
-@route('/')
-def index():
-    return '''
-          <div>
-          <a href='/sida/about'>About</a> </b><a href='/sida/bio'>Biography</a> <a href='/sida/pic'>Pictures</a>
-          </div>
-          '''
 
-@route('/sida/<id>')
-def index(id):
-    return "Her er ", id, " sidan"
-
-def about():
-    return "Hallo hesugndjonk"
-
-@route('/gaman')
-def index():
-    x = request.query.tala
-    return "Þetta er gildi breytunnar tala", x
-
-@error(404)
-def villa(error):
-    return "<h1>Þessi síða er ekki til</h1>"
-"""
 
