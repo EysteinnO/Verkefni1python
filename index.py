4459efdc87e85b5082ba1175d6a9677b2fff6246
Eysteinn Orri Sigurðsson <eysteinnsi227@nemi.tskoli.is>
2018-01-19 14:09:43 +0000

    iii

---
+++ index.py
@@ -0,0 +1,27 @@
+# -*- coding: cp1252 -*-
+from bottle import *
+
+@route('/')
+def index():
+    return '''
+          <div>
+          <a href='/sida/about'>About</a> </b><a href='/sida/bio'>Biography</a> <a href='/sida/pic'>Pictures</a>
+          </div>
+          '''
+
+@route('/sida/<id>')
+def index(id):
+    return "Her er ", id, " sidan"
+
+def about():
+    return "Hallo hesugndjonk"
+
+@route('/gaman')
+def index():
+    x = request.query.tala
+    return "Þetta er gildi breytunnar tala", x
+
+@error(404)
+def villa(error):
+    return "<h1>Þessi síða er ekki til</h1>"
+run(host="localhost", port=8080);
